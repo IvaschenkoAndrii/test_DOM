4c683cbbc029ea79f1edd781645616aac13c3dd4
Андрей <ivaschenko84@gmail.com>
2023-01-12 16:25:43 +0100

    test DOM

---
+++ hello.js
@@ -0,0 +1,27 @@
+let title = document.getElementsByClassName('item-grid-card__title')
+let authors = document.getElementsByClassName('item-grid-music-preview__author')
+let mp3 = document.getElementsByTagName('audio')
+
+let obj =
+    {
+        title: null,
+        author: null,
+        mp3: null
+    }
+
+
+let res = [];
+
+for (let i = 0; i < title.length; i++) {
+
+    let obj =
+        {
+            title: title[i].innerText,
+            author: authors[i].innerText,
+            mp3: mp3[i].src
+        }
+
+    res.push(obj)
+}
+
+console.log(res);
